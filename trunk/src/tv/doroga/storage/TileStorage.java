dc432119f519fca8631da6cda48271b281ce22c7
nnseva <nnseva@a8e7ab30-c72c-96ef-150c-b7f8cf85fda7>
2010-09-22 00:27:25 +0000

    The tile cache (not used yet)

--- trunk/src/tv/doroga/storage/TileStorage.java
+++ trunk/src/tv/doroga/storage/TileStorage.java
@@ -3,5 +3,9 @@ package tv.doroga.storage;
 import android.content.Context;
 
 public interface TileStorage {
-	public byte[] GetTile(Context context, int tilex, int tiley, int tilezoom);
+	public interface TileReceiver {
+		void TileGot(int tilex, int tiley, int tilezoom, byte[] tilecontent, long tiletimestamp);
+		void TileUnvailable(int tilex, int tiley, int tilezoom);
+	}
+	void RequestTile(int tilex, int tiley, int tilezoom, TileReceiver receiver);
 }
